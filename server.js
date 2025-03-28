1f04e75cb50a842b138e269d2e61e584b7cad932
Prabhu <davalavinoo@gmail.com>
2025-03-28 20:44:28 +0530

    Add CORS for Vercel frontend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://expense-tracker-frontend-harshika.vercel.app' }));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
