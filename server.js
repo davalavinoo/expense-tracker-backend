690d41f7ab9dd8ca7938ef1d7f8b5e57430abd1b
Prabhu <davalavinoo@gmail.com>
2025-03-31 09:38:48 +0530

    Update CORS for new frontend URL-1

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: [
-'https://expense-tracker-frontend-harshikaclean-davala-vinod-abrahams-projects.vercel.app' }));
-'http://localhost:3000']
+'https://expense-tracker-frontend-harshikaclean-davala-vinod-abrahams-projects.vercel.app',
+'http://localhost:3000']}));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
